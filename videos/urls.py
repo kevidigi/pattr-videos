cd604bf22f9ff9b0bb7811a9fcdb14c0304c5535
kevidigi <kevin.e.lynch89@gmail.com>
2021-06-19 22:18:02 +0100

    first commit/edited allauth templates

---
+++ videos/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.CreateVideo.as_view(), name='video-create'),
+    path('<int:pk>/', views.DetailVideo.as_view(), name='video-detail'),
+    path('<int:pk>/edit', views.UpdateVideo.as_view(), name='video-update'),
+    path('<int:pk>/delete', views.DeleteVideo.as_view(), name='video-delete'),
+    ]
