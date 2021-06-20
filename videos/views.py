03bfdd5e5338dbc0944220c59e346d11c99d6731
kevidigi <kevin.e.lynch89@gmail.com>
2021-06-20 22:25:49 +0100

    detail loading, vid not playing

--- videos/views.py
+++ videos/views.py
@@ -4,15 +4,20 @@ from django.views.generic.detail import DetailView
 from .models import Video
 from django.urls import reverse
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render(request, 'videos/index.html')
 
 
-class CreateVideo(CreateView):
+class CreateVideo(LoginRequiredMixin, CreateView):
     model = Video
     fields = ['title', 'description', 'video_file', 'thumbnail']
     template_name = 'videos/create_video.html'
+    
+    def form_valid(self, form):
+        form.instance.uploader = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('video-detail', kwargs={'pk': self.object.pk})
@@ -23,7 +28,7 @@ class DetailVideo(DetailView):
     template_name = 'videos/detail_video.html'
 
 
-class UpdateVideo(UpdateView):
+class UpdateVideo(LoginRequiredMixin, UpdateView):
     model = Video
     fields = ['title', 'description']
     template_name = "videos/create_video.html"
@@ -32,7 +37,7 @@ class UpdateVideo(UpdateView):
         return reverse('video-detail', kwargs={'pk': self.object.pk})
 
 
-class DeleteVideo(DeleteView):
+class DeleteVideo(LoginRequiredMixin, DeleteView):
     model = Video
     template_name = 'videos/delete_video.html'
 
